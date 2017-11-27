c7708595aa8ec35d3b6b17c012e2b6a5b1ead6e0
Roman Yatsyna <alpha7animal@gmail.com>
2017-11-27 23:36:03 +0300

    Running tests in parallel. Each test runner is running in its own thread. Threads count is specified in POM file in maven-failsafe-plugin.

--- src/test/java/com/ryatsyna/DictionaryTestRunner.java
+++ src/test/java/com/ryatsyna/DictionaryTestRunner.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/wiki_page")
-public class DemoTestRunner {}
+@CucumberOptions(features="src/test/resources/features/consult_dictionary")
+public class DictionaryTestRunner {}
