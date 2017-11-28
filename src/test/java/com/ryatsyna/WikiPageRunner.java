1f84bd852ad7450bb54056b8005259537ba7b78e
Roman Yatsyna <alpha7animal@gmail.com>
2017-11-28 03:16:16 +0300

    Parallel test execution without need to create a test runner for each feature file was added. Doesn't work with russian language. Wait till cucumber-jvm-parallel-plugin 4.2.1 will be released.

--- src/test/java/com/ryatsyna/WikiPageRunner.java
+++
@@ -1,9 +0,0 @@
-package com.ryatsyna;
-
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/wiki_page")
-public class WikiPageRunner {}
